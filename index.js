7fd5a01b9d57bff4890c9b7f490a994a6586406a
harshavardhan penumala <penumalaharshavardhan@gmail.com>
2025-01-07 09:21:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-let clockEl = document.getElementById("clock");
+const hourHand = document.getElementById("hour-hand");
+const secondsHand = document.getElementById("seconds-hand");
+const minuteHand = document.getElementById("minute-hand");
 
-function Clock(){
-    clockEl = new Date();
-
+function setClock(){
+    const date = new Date();
+    const seconds = date.getSeconds() / 60;
+    const minutes = (seconds + date.getMinutes()) / 60;
+    const hours = (minutes + date.getHours()) / 12;
+    
+    rotateClockHand(secondsHand,seconds);
+    rotateClockHand(minuteHand,minutes);
+    rotateClockHand(hourHand, hours);
 }
-setInterval(Clock,1000);
 
-Clock();
+function rotateClockHand(element , rotation){
+    element.style.setProperty('--rotate' , rotation * 360);
+}
+
+setInterval(setClock , 1000);
